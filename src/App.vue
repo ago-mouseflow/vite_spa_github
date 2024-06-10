82cca10ae44513a6ca87f0c0cc020a27bfb43bd2
Alexander Goobar <ago@mouseflow.com>
2024-06-10 16:51:18 +0200

    Change logo

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://mouseflow.com" target="_blank">
+      <img src="./assets/logo.svg" class="logo" alt="Mouseflow logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue TEST" />
+  <HelloWorld msg="Hey Flowsters" />
 </template>
 
 <style scoped>
